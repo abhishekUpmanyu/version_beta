3de0cb5e4cac532e01be7f3d2c2c81015e0d4778
Abhishek Upmanyu <upmanyu.abhishek@yahoo.in>
2019-10-13 10:29:28 +0530

    finally done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:version_beta/pages/home.dart';
-import 'package:version_beta/pages/login.dart';
-import 'package:version_beta/pages/sign_up.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MaterialApp(home: Home()));
